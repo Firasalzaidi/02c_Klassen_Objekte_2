dd2a945c43998ac79433a19016493ae259762cc1
Firas <firas.allzaydy@gmail.com>
2024-01-26 10:50:53 +0100

    boolean / isFemale ++

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -5,29 +5,33 @@ public class Cat {
   private String name;
   private String furColor;
   private int age; 
+  private boolean isFemale;
 
-
-public Cat(String name, String furColor, int age) {
+  public Cat(String name, String furColor, int age, boolean isFemale) {
     this.name = name;
     this.furColor = furColor;
     this.age = age;
+    this.isFemale = isFemale;
   }
 
-public String getStringAttributes(String op) {
-  switch (op) {
-    case "#name":
-      return name;
-    case "#color":
-      return furColor;
-    default:
-      return "ERROR!";
+  public String getStringAttributes(String op) {
+    switch (op) {
+      case "#name":
+        return name;
+      case "#color":
+        return furColor;
+      default:
+        return "ERROR!";
+    }
   }
+
+  public String getAge() {
+    if (isFemale) {
+      return "This is an inappropriate question!";
+    } else {
+      return String.valueOf(age);
+    }
+  }
+
+
 }
-
-public String getAge() {
-    return String.valueOf(age);
-  }
-
-
-}
-  
